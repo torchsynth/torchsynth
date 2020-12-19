eeddb1e93c791669cc03e6616057e588089b9f43
maxsolomonhenry <max.henry@mail.mcgill.ca>
2020-12-19 00:02:24 -0500

    added VCO

--- ddrum/drum_engine.py
+++ ddrum/drum_engine.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import resample as resample
 from parameters import SAMPLE_RATE, CONTROL_RATE
 
 
@@ -111,3 +112,57 @@ class ADSR:
 
     def play(self, dur):
         return np.concatenate((self.note_on(), np.full(dur*CONTROL_RATE, self.get_s()), self.note_off()))
+
+
+class VCO:
+    """
+    Voltage controlled oscillator. Accepts control rate instantaneous frequency, outputs audio.
+
+    TODO: more than just cosine.
+
+    Parameters
+    ---------
+
+    None.
+
+    Examples
+    --------
+
+    >>> myVCO = VCO()
+    >>> two_8ve_chirp = myVCO(np.linspace(440, 1760, 1000))
+
+    """
+
+    def __init__(self):
+        self.__f0 = []
+        self.__phase = 0
+
+    def __call__(self, f0):
+        self.set_f0(f0)
+        return self.play()
+
+    def get_f0(self):
+        return self.__f0
+
+    def set_f0(self, val):
+        nyq = SAMPLE_RATE // 2
+        val[np.where(val < 0)] = 0
+        val[np.where(val > nyq)] = nyq
+        self.__f0 = val
+
+    def get_phase(self):
+        return self.__phase
+
+    def set_phase(self, val):
+        self.__phase = val % (2 * np.pi)
+
+    def play(self):
+        arg = self.to_arg(self.get_f0()) + self.get_phase()
+        self.set_phase(arg[-1])
+        return np.cos(arg)
+
+    @staticmethod
+    def to_arg(f0):
+        out_length = len(f0) * SAMPLE_RATE / CONTROL_RATE
+        up_sampled = resample(f0, int(out_length))
+        return np.cumsum(2 * np.pi * up_sampled / SAMPLE_RATE)
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,25 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from ddrum.drum_engine import ADSR
+from ddrum.drum_engine import ADSR, VCO
+from scipy.signal import stft as stft
 
-# myADSR = ADSR(0.5, 0.5, 0.5, 0.5, 3)
+# Envelope test
 myADSR = ADSR()
 plt.plot(myADSR(1))
+plt.title('a={}, d={}, s={}, r={}, alpha={}'.format(myADSR.get_a(),
+                                                    myADSR.get_d(),
+                                                    myADSR.get_s(),
+                                                    myADSR.get_r(),
+                                                    myADSR.get_alpha()))
+plt.show()
+
+# VCO test
+test_f0 = myADSR(1)*20000
+myVCO = VCO()
+
+x = myVCO(test_f0)
+mX = np.abs(stft(x)[2])
+
+plt.imshow(mX, origin='lower', aspect='auto')
+plt.title('VCO output')
 plt.show()
