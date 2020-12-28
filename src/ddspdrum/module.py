de225c253c4bec9f6bbaaf7a027c91d430c7953c
maxsolomonhenry <max.henry@mail.mcgill.ca>
2020-12-28 11:26:01 -0500

    refactored to use midi

--- src/ddspdrum/module.py
+++ src/ddspdrum/module.py
@@ -5,12 +5,10 @@ Synth modules.
                 - Convert operations to tensors, obvs.
 """
 
-import copy
-
 import numpy as np
 from scipy.signal import resample
 
-from ddspdrum.defaults import CONTROL_RATE, SAMPLE_RATE
+from ddspdrum.defaults import CONTROL_RATE, SAMPLE_RATE, EPSILON
 
 
 class SynthModule:
@@ -18,11 +16,10 @@ class SynthModule:
     Base class for synthesis modules. Mostly helper functions for the moment.
     """
 
-    def __init__(
-        self, sample_rate: int = SAMPLE_RATE, control_rate: int = CONTROL_RATE
-    ):
+    def __init__(self):
         self.sample_rate = SAMPLE_RATE
         self.control_rate = CONTROL_RATE
+        self.eps = EPSILON
 
     def control_to_sample_rate(self, control: np.array) -> np.array:
         """
@@ -37,11 +34,23 @@ class SynthModule:
         """
         # Right now it appears that all signals are 1d, but later
         # we'll probably convert things to 2d: instance x signal
-        assert control.ndim == 1
-        nsamples = int(round(len(control) * self.sample_rate / self.control_rate))
-        return resample(control, nsamples)
 
-    def fix_length(self, signal: np.array, length: int) -> np.array:
+        if self.control_rate == self.sample_rate:
+            return control
+        else:
+            assert control.ndim == 1
+            num_samples = int(round(len(control) * self.sample_rate / self.control_rate))
+            return resample(control, num_samples)
+
+    def hz_to_midi(self, hz):
+        return 12 * np.log2((hz + self.eps) / 440) + 69
+
+    @staticmethod
+    def midi_to_hz(midi):
+        return 440.0 * (2.0 ** ((midi - 69.0) / 12.0))
+
+    @staticmethod
+    def fix_length(signal: np.array, length: int) -> np.array:
         # Right now it appears that all signals are 1d, but later
         # we'll probably convert things to 2d: instance x signal
         assert signal.ndim == 1
@@ -65,8 +74,6 @@ class ADSR(SynthModule):
         s: float = 0.5,
         r: float = 0.5,
         alpha: float = 3.0,
-        sample_rate: int = SAMPLE_RATE,
-        control_rate: int = CONTROL_RATE,
     ):
         """
         Parameters
@@ -79,7 +86,8 @@ class ADSR(SynthModule):
         r                   :   release time (sec), >= 0
         alpha               :   envelope curve, >= 0. 1 is linear, >1 is exponential.
         """
-        super().__init__(sample_rate=sample_rate, control_rate=control_rate)
+
+        super().__init__()
         assert alpha >= 0
         self.alpha = alpha
 
@@ -95,24 +103,47 @@ class ADSR(SynthModule):
         assert r >= 0
         self.r = r
 
-    def __call__(self, sustain_duration):
+    def __call__(self, sustain_duration=0):
+        """ Generate an envelope that sustains for a given duration in seconds.
+
+        Generates a control-rate envelope signal with given attack, decay and release times, sustained for
+        `sustain_duration` in seconds. E.g., an envelope with no attack or decay, a sustain duration of 1 and a 0.5
+        release will last for 1.5 seconds.
+
         """
-        Play, for some duration in seconds.
-        """
+
         assert sustain_duration >= 0
         return np.concatenate(
             (
-                # Is this the DSP-way of thinking about it, (note on and note off)
-                # or can we change this just to self.attack, self.decay, etc.
                 self.note_on,
-                # This makes me believe that duration should maybe be a class value
-                # defined in __init__. How common is it to create a module and
-                # with fixed settings and use it several times with different
-                # duration?
                 self.sustain(sustain_duration),
                 self.note_off,
             )
         )
+
+    def _ramp(self, duration: float):
+        """ Makes a ramp of a given duration in seconds, returned at control rate.
+
+        This function is used for the piece-wise construction of the envelope signal. Its output monotonically
+        increases from 0 to 1. As a result, each component of the envelope is a scaled and possibly reversed version of
+        this ramp:
+
+        attack      -->     returns an `a`-length ramp, as is.
+        decay       -->     `d`-length reverse ramp, scaled and shifted to descend from 1 to `s`.
+        release     -->     `r`-length reverse ramp, scaled and shifted to descend from `s` to 0.
+
+        Its curve is determined by alpha:
+
+        alpha = 1 --> linear,
+        alpha > 1 --> exponential,
+        alpha < 1 --> logarithmic.
+
+        """
+
+        t = np.linspace(
+            0, duration, int(round(duration * self.control_rate)), endpoint=False
+        )
+        return (t / duration) ** self.alpha
 
     @property
     def attack(self):
@@ -120,7 +151,7 @@ class ADSR(SynthModule):
 
     @property
     def decay(self):
-        # TODO: This is a bit obtuse and would be great to explain
+        # `d`-length reverse ramp, scaled and shifted to descend from 1 to `s`.
         return self._ramp(self.d)[::-1] * (1 - self.s) + self.s
 
     def sustain(self, duration):
@@ -128,7 +159,7 @@ class ADSR(SynthModule):
 
     @property
     def release(self):
-        # TODO: This is a bit obtuse and would be great to explain
+        # `r`-length reverse ramp, scaled and shifted to descend from `s` to 0.
         return self._ramp(self.r)[::-1] * self.s
 
     @property
@@ -139,67 +170,82 @@ class ADSR(SynthModule):
     def note_off(self):
         return self.release
 
-    def _ramp(self, duration: float):
-        """
-        Create a ramp function for a certain duration in seconds,
-        applying the envelope curve and returning it in control rate.
-        """
-        t = np.linspace(
-            0, duration, int(round(duration * self.control_rate)), endpoint=False
-        )
-        return (t / duration) ** self.alpha
-
     def __str__(self):
         return (
-            f"ADRS(a={self.a}, d={self.d}, s={self.s}, r={self.r}, alpha={self.alpha})"
+            f"ADSR(a={self.a}, d={self.d}, s={self.s}, r={self.r}, alpha={self.alpha})"
         )
 
 
 class VCO(SynthModule):
     """
-    Voltage controlled oscillator. Accepts control rate instantaneous frequency, outputs audio.
+    Voltage controlled oscillator. Is called with control-rate pitch modulation, outputs audio.
 
-    TODO: more than just cosine.
+    Think of this as a VCO on a modular synthesizer. It has a base pitch (specified here as a midi value), and a pitch
+    modulation depth. Its call accepts a control-rate modulation signal between 0 - 1. An array of 0's returns a
+    stationary audio signal at its base pitch.
+
+
+    Parameters
+    ----------
+
+    midi_f0 (flt)       :       pitch value in 'midi' (69 = 440Hz).
+    mod_depth (flt)     :       depth of the pitch modulation; 0 means no modulation.
+
+    TODO:   - more than just cosine.
 
     Examples
     --------
 
-    >>> myVCO = VCO()
-    >>> two_8ve_chirp = myVCO(np.linspace(440, 1760, 1000))
+    >>> myVCO = VCO(midi_f0=69, mod_depth=24)
+    >>> two_8ve_chirp = myVCO(np.linspace(0, 1, 1000, endpoint=False))
     """
 
     def __init__(
-        self, sample_rate: int = SAMPLE_RATE, control_rate: int = CONTROL_RATE
+        self, midi_f0: float = 69, mod_depth: float = 1, phase: float = 0
     ):
-        super().__init__(sample_rate=sample_rate, control_rate=control_rate)
-        self.__f0 = []
-        self.__phase = 0
+        super().__init__()
 
-    def __call__(self, f0control: np.array, phase: float):
-        # TODO: Are there expected ranges on phase?
-        # Does it matter? Can we just assert preconditions here?
-        phase = phase % (2 * np.pi)
+        assert 0 <= midi_f0 <= 127
+        self.midi_f0 = midi_f0
 
-        nyquist = self.sample_rate // 2
-        # I'm still a bit concerned about using clip,
-        # because we lose the gradients from it, but let's worry
-        # about that later.
-        clampedf0control = np.clip(f0control, 0, nyquist)
+        assert mod_depth >= 0
+        self.mod_depth = mod_depth
 
-        # What is arg and can we give it a better name?
-        arg = self.to_arg(clampedf0control) + phase
-        # Why do we do a `set_phase` here at the end?
-        # If this is really some sort of recurrent state then maybe
-        # we return it and pass it back in?
-        # self.set_phase(arg[-1])
-        return np.cos(arg)
+        self.phase = phase
 
-    def to_arg(self, f0control: np.array):
-        # Can we describe what this does and give it a better name?
-        # I see that it upsamples from control to sample rate.
-        # Then it does a cumulative sum?? So it's to modulate the
-        # pitch over time?
-        up_sampled = self.control_to_sample_rate(f0control)
+    def __call__(self, mod_signal: np.array, phase: float):
+        """ Generates audio signal from control-rate mod.
+
+        There are three representations of the 'pitch' at play here: (1) midi, (2) instantaneous frequency, and
+        (3) phase, a.k.a. 'argument'.
+
+        (1) midi    This is an abuse of the standard midi convention, where semitone pitches are mapped from 0 - 127.
+                    Here it's a convenient way to represent pitch linearly. An A above middle C is midi 69.
+
+        (2) freq    Pitch scales logarithmically in frequency. An A above middle C is 440Hz.
+
+        (3) phase   This is the argument of the cosine function that generates sound. Frequency is the first derivative
+                    of phase; phase is integrated frequency (~ish).
+
+        First we generate the 'pitch contour' of the signal in midi values (mod contour + base pitch). Then we convert
+        to a phase argument (via frequency), then output sound.
+
+        """
+
+        assert (mod_signal >= 0).all() and (mod_signal <= 1).all()
+
+        control_as_midi = self.mod_depth * mod_signal + self.midi_f0
+        control_as_frequency = self.midi_to_hz(control_as_midi)
+        cosine_argument = self.make_argument(control_as_frequency) + phase
+
+        self.phase = cosine_argument[-1]
+        return np.cos(cosine_argument)
+
+    def make_argument(self, control_as_frequency: np.array):
+        """ Generates the phase argument to feed a cosine function in order to generate audio.
+        """
+
+        up_sampled = self.control_to_sample_rate(control_as_frequency)
         return np.cumsum(2 * np.pi * up_sampled / SAMPLE_RATE)
 
 
@@ -208,10 +254,8 @@ class VCA(SynthModule):
     Voltage controlled amplifier. Shapes amplitude of audio rate signal with control rate level.
     """
 
-    def __init__(
-        self, sample_rate: int = SAMPLE_RATE, control_rate: int = CONTROL_RATE
-    ):
-        super().__init__(sample_rate=sample_rate, control_rate=control_rate)
+    def __init__(self):
+        super().__init__()
         self.__envelope = np.array([])
         self.__audio = np.array([])
 
