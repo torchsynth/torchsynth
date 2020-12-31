34f61f49640eb895620fce2bfc658ce8ffe580b4
maxsolomonhenry <max.henry@mail.mcgill.ca>
2020-12-30 22:48:40 -0500

    examples with squaresaw

--- examples/examples.py
+++ examples/examples.py
@@ -14,7 +14,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-from ddspdrum.module import ADSR, VCA, VCO, Drum
+from ddspdrum.module import ADSR, VCA, VCO, SquareSaw, Drum
 
 # -
 
@@ -70,6 +70,28 @@ plt.show()
 ipd.Audio(vco_out, rate=vco.sample_rate)
 # -
 
+# Check this out, it's a square / saw oscillator. Use the shape parameter to interpolate between a square wave (shape = 0) and a sawtooth wave (shape = 1).
+
+# +
+# SquareSaw test: shape 0 --> square, 1 --> saw.
+
+shape = 0
+
+midi_f0 = 55
+sqs = SquareSaw(shape=shape, midi_f0=midi_f0, mod_depth=6)
+sqs_out = sqs(envelope, phase=0)
+
+# +
+X = librosa.stft(sqs_out)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(5, 5))
+librosa.display.specshow(Xdb, sr=vco.sample_rate, x_axis="time", y_axis="hz")
+plt.ylim(0, 5000)
+plt.show()
+
+ipd.Audio(sqs_out, rate=vco.sample_rate)
+# -
+
 # Notice that this sound is rather clicky. We'll add an envelope to the amplitude to smooth it out.
 
 # VCA test
@@ -95,18 +117,45 @@ ipd.Audio(vca_out, rate=vca.sample_rate)
 # Alternately, you can just use the Drum class that composes all these modules together automatically.
 
 # +
-my_drum = Drum()
+my_drum = Drum(
+    pitch_adsr=ADSR(0.25, 0.25, 0.25, 0.25, alpha=3),
+    amp_adsr=ADSR(0.25, 0.25, 0.25, 0.25),
+    vco=VCO(midi_f0=69, mod_depth=12),
+    sustain_duration=1
+    )
 
-boom = my_drum()
+drum_out = my_drum()
 
-X = librosa.stft(boom)
+X = librosa.stft(drum_out)
 Xdb = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize=(5, 5))
 librosa.display.specshow(Xdb, sr=vca.sample_rate, x_axis="time", y_axis="hz")
 plt.ylim(0, 2000)
 plt.show()
 
-ipd.Audio(boom, rate=vca.sample_rate)
+ipd.Audio(drum_out, rate=vca.sample_rate)
 # -
+# You can also use the **SquareSaw oscillator** in the drum module.
+
+# +
+my_drum = Drum(
+    pitch_adsr=ADSR(0.25, 0.25, 0.25, 0.25, alpha=3),
+    amp_adsr=ADSR(0.25, 0.25, 0.25, 0.25),
+    vco=SquareSaw(shape=0, midi_f0=24, mod_depth=12),
+    sustain_duration=1
+    )
+
+drum_out = my_drum()
+
+X = librosa.stft(drum_out)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(5, 5))
+librosa.display.specshow(Xdb, sr=vca.sample_rate, x_axis="time", y_axis="hz")
+plt.ylim(0, 5000)
+plt.show()
+
+ipd.Audio(drum_out, rate=vca.sample_rate)
+# -
+
 
 
