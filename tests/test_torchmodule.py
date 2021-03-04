bc2be7e69420d345085351628e59bb80ebdf7394
Jordie Shier <jordieshier@gmail.com>
2021-03-04 15:25:53 -0800

    Add some tests for TorchSynth

--- tests/test_torchmodule.py
+++ tests/test_torchmodule.py
@@ -7,11 +7,13 @@ from typing import Any, Callable, Dict
 
 import numpy as np
 import pytest
+import torch.nn
 import torch.tensor as T
 
 import ddspdrum.module as numpymodule
 import ddspdrum.torchmodule as torchmodule
 from ddspdrum.parameter import ParameterRange, TorchParameter
+import ddspdrum.defaults as defaults
 
 random.seed(0)
 
@@ -196,3 +198,59 @@ class TestTorchSynthModule:
         module.add_parameters([param_1])
         assert module.torchparameters["param_1"] == 0.25
         assert module.p("param_1") == 5000.0
+
+
+class TestTorchSynth():
+    """
+    Tests for TorchSynth
+    """
+
+    def test_construction(self):
+        # Test empty construction
+        synth = torchmodule.TorchSynth()
+        assert synth.sample_rate == defaults.SAMPLE_RATE
+        assert synth.buffer_size == defaults.BUFFER_SIZE
+
+        # Test construction with args
+        synth = torchmodule.TorchSynth(sample_rate=16000, buffer_size=512)
+        assert synth.sample_rate == 16000
+        assert synth.buffer_size == 512
+
+    def test_add_synth_module(self):
+
+        synth = torchmodule.TorchSynth()
+        vco = torchmodule.TorchSineVCO()
+        noise = torchmodule.TorchNoise()
+
+        synth.add_synth_modules({
+            "vco": vco,
+            "noise": noise
+        })
+        assert hasattr(synth, "vco")
+        assert hasattr(synth, "noise")
+
+        # Make sure all the parameters were registered correctly
+        synth_params = [p for p in synth.parameters()]
+        module_params = [p for p in vco.parameters()]
+        module_params.extend([p for p in noise.parameters()])
+        for p in module_params:
+            assert p in synth_params
+
+        # Expect a TypeError if a non TorchSynthModule is passed in
+        with pytest.raises(TypeError):
+            synth.add_synth_modules({
+                'module': torch.nn.Module()
+            })
+
+        # Expect a ValueError if the incorrect sample rate or buffer size is passed in
+        with pytest.raises(ValueError):
+            vco_2 = torchmodule.TorchSineVCO(sample_rate=16000)
+            synth.add_synth_modules({
+                'vco_2': vco_2
+            })
+
+        with pytest.raises(ValueError):
+            adsr = torchmodule.TorchADSR(buffer_size=512)
+            synth.add_synth_modules({
+                'adsr': adsr
+            })
