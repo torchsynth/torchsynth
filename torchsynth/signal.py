0828b467122d78f4d10b637c3415196aadf38417
Joseph Turian <turian@gmail.com>
2021-03-07 18:56:03 +0100

    Reformatted

--- torchsynth/signal.py
+++ torchsynth/signal.py
@@ -4,13 +4,14 @@ A convenience type for signals.
 
 import torch
 
+
 class Signal(torch.Tensor):
     @staticmethod
     def __new__(cls, x, *args, **kwargs):
         return super().__new__(cls, x, *args, **kwargs)
 
     def __init__(self, x):
-        super().__init__(x) # optional
+        super().__init__(x)  # optional
 
     @property
     def batch_size(self):
