f756f3ca09a4ca9b8139fdea3b54c3a544fc512e
Joseph Turian <turian@gmail.com>
2021-03-10 00:56:00 +0100

    Add lightning

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # },
     scripts=[],
     python_requires=">=3.6",
-    install_requires=["numpy", "scipy", "torch>=1.7"],
+    install_requires=["numpy", "scipy", "torch>=1.7", "pytorch-lightning"],
     extras_require={
         "test": [
             "pytest",
