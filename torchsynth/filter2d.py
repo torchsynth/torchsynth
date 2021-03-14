1b5f37665b999b5312b83a3c6b56e80e49ae921a
maxsolomonhenry <max.henry@mail.mcgill.ca>
2021-03-13 23:33:39 -0500

    Time-varying, has clicks

--- torchsynth/filter2d.py
+++ torchsynth/filter2d.py
@@ -29,56 +29,126 @@ import torchsynth.util as util
 
 class Dummy:
     SR = 44100
-    frame_size = 4096
+    frame_size = 512
 
 
 class VCF1d(Dummy):
     def __call__(self, signal: T, cutoff: T):
 
         # Pad or cut to match length.
-        num_samples = cutoff * self.frame_size
-        signal = util.fix_length(num_samples)
+        num_samples = len(cutoff) * self.frame_size
+        signal = util.fix_length2D(signal, num_samples)
 
         framed_signal = self.make_framed(signal)
         impulse_matrix = self.get_impulse_matrix_1d(cutoff)
-        return framed_signal
+        convolved_frames = self.fast_convolve(framed_signal, impulse_matrix)
+
+        y = self.overlap_add(convolved_frames)
+
+        return y
 
     def make_framed(self, signal):
         frame_size = self.frame_size
-        num_frames = signal.size // frame_size  # TODO
-        return torch.reshape(signal, [frame_size, num_frames])
+        num_frames = signal.shape[1] // frame_size  # TODO
+        return signal.view(num_frames, frame_size).T
+
+    @staticmethod
+    def fast_convolve(input1, input2):
+        """
+        Multiply spectra of two signals. Returns array of overlapping frames.
+        """
+        # For convenience, does not have to be strictly true for this technique.
+        assert input1.shape == input2.shape
+
+        frame_length, num_frames = input1.shape
+        fft_size = util.next_power_of_two(frame_length) * 2
+
+        # out_ = torch.zeros([fft_size, num_frames])
+
+        fft1 = torch.fft.rfft(input1, n=fft_size, axis=0)
+        fft2 = torch.fft.rfft(input2, n=fft_size, axis=0)
+        tmp = fft1 * fft2
+
+        return torch.fft.irfft(tmp, axis=0)
+
+    @staticmethod
+    def overlap_add(signal: T) -> T:
+        """
+        Take signal array of 2x overlapping frames and sum to 1D.
+        """
+
+        frame_size, num_frames = signal.shape
+
+        # Assumes overlap by two. Maybe this is a bad idea for the future.
+        hop_size = frame_size // 2
+
+        samples_out = num_frames * hop_size + (frame_size - hop_size)
+        out_ = torch.zeros(samples_out)
+
+        signal = torch.roll(signal, -hop_size // 2 - 1, 0)
+
+        a = torch.flatten(signal[hop_size:, :].T)
+        b = torch.flatten(signal[:hop_size, :].T)
+
+        out_[:-hop_size] += a
+        out_[hop_size:] += b
+
+        return out_
 
     def get_impulse_matrix_1d(self, cutoff: T) -> T:  # 1 x num_frames
         """
         Make matrix of impulse responses for time-varying LP filter.
         """
 
-        cutoff = torch.arange(440, 880)
-        window = torch.hann_window(self.frame_size)
+        window = torch.hamming_window(self.frame_size)
 
         hN = (self.frame_size / self.SR) / 2
         t = torch.arange(self.frame_size) / self.SR
         t = t - hN
 
-        impulse = torch.sinc(2 * cutoff * t[:, None])
+        impulse = (2 * cutoff) * torch.sinc(2 * cutoff * t[:, None])
         impulse = impulse * window[:, None]
-        return torch.fft.fftshift(impulse)  # frame_size x num_frames
 
+        return impulse
 
-# Single impulse
-
-# Impulse matrix
-
-# Time varying impulse matrix
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    from torchsynth.module import SquareSawVCO
+    from torchsynth.module import SquareSawVCO, MonophonicKeyboard
     import torchsynth.globals
 
+    def my(signal):
+        return signal.detach().numpy()
+
     synthglobals = torchsynth.globals.SynthGlobals(
-        sample_rate=T(44100), buffer_size=T(44100), batch_size=T(1)
+        sample_rate=T(44100), buffer_size=T(88200), batch_size=T(1)
     )
 
-    vco = SquareSawVCO(midi_f0=T([30]), shape=T([1]), synthglobals=synthglobals)
-    out = vco(torch.ones(44100))
+    keyboard = MonophonicKeyboard(synthglobals, midi_f0=T([30.0]))
+
+    square_saw = SquareSawVCO(
+        tuning=T([0.0]),
+        mod_depth=T([1.0]),
+        shape=T([1.0]),
+        synthglobals=synthglobals,
+    )
+    env2 = torch.zeros([1, square_saw.buffer_size])
+
+    out_ = square_saw(keyboard.p("midi_f0"), env2)
+
+    vcf = VCF1d()
+
+    num_frames = 88200 // 512
+    cutoff = torch.linspace(1.0, 0.0, num_frames) ** 4 * 5000 + 100
+
+    my_out = vcf(out_, cutoff)
+
+    x = my_out.detach().numpy()
+
+    import IPython.display as ipd
+
+    display(ipd.Audio(x, rate=44100))
+
+    plt.plot(x[:20000])
+
+num_frames
--- torchsynth/util.py
+++ torchsynth/util.py
@@ -23,36 +23,51 @@ def amplitude_to_db(amplitude: T, amin: T = T(1e-10)) -> T:
     return 20 * torch.log10(torch.max(amplitude, amin))
 
 
+def blackman(length: T) -> T:
+    num_samples = torch.ceil(length)
+    diff = num_samples - length
+    n = torch.arange(num_samples.detach() - (diff.detach() / 2), device=length.device)
+    cos_a = torch.cos(2 * math.pi * n / (length - 1))
+    cos_b = torch.cos(4 * math.pi * n / (length - 1))
+    window = 0.42 - 0.5 * cos_a + 0.08 * cos_b
+
+    # Linearly interpolate the ends of the window to achieve fractional length
+    window = torch.cat(
+        (
+            T([0.0 * diff + window[0] * (1.0 - diff)], device=length.device),
+            window[1:-1],
+            T([0.0 * diff + window[-1] * (1.0 - diff)], device=length.device),
+        )
+    )
+
+    return window
+
+
+def crossfade(in_1: T, in_2: T, ratio: T) -> T:
+    """
+    Equal power cross-fade.
+    """
+    assert 0.0 <= ratio <= 1.0
+    return EQ_POW * (torch.sqrt(1 - ratio) * in_1 + torch.sqrt(ratio) * in_2)
+
+
+def crossfade2D(in_1: Signal, in_2: Signal, ratio: T) -> Signal:
+    """
+    Equal power cross-fade.
+
+    TODO: Replace crossfade with this once everything is 2D
+    """
+    assert in_1.ndim == 2 and in_2.ndim == 2 and ratio.ndim == 1
+    assert torch.all(0.0 <= ratio) and torch.all(ratio <= 1.0)
+    ratio = ratio.unsqueeze(1)
+    return EQ_POW * (torch.sqrt(1 - ratio) * in_1 + torch.sqrt(ratio) * in_2)
+
+
 def db_to_amplitude(db: T) -> T:
     """
     Convert decibel value to an amplitude between 0 and 1
     """
     return torch.pow(10, db / 20)
-
-
-def peak_gain_for_Q(Q: T) -> T:
-    """
-    Calculate the peak gain for a given filter quality factor.
-    """
-    # No gain added for quality factor less then 1/sqrt(2)
-    if Q <= 0.707:
-        return T(1.0)
-
-    return Q * Q / torch.pow((Q * Q - 0.25), 0.5)
-
-
-def hz_to_midi(hz: T) -> T:
-    """
-    Convert from frequency in Hz to midi (linear pitch).
-    """
-    return 12 * torch.log2((hz + EPS) / 440) + 69
-
-
-def midi_to_hz(midi: T) -> T:
-    """
-    Convert from midi (linear pitch) to frequency in Hz.
-    """
-    return 440.0 * (2.0 ** ((midi - 69.0) / 12.0))
 
 
 def fix_length(signal: T, length: Union[int, T]) -> T:
@@ -92,24 +107,11 @@ def fix_length2D(signal: Signal, length: Union[int, T]) -> Signal:
     return signal
 
 
-def crossfade(in_1: T, in_2: T, ratio: T) -> T:
+def hz_to_midi(hz: T) -> T:
     """
-    Equal power cross-fade.
+    Convert from frequency in Hz to midi (linear pitch).
     """
-    assert 0.0 <= ratio <= 1.0
-    return EQ_POW * (torch.sqrt(1 - ratio) * in_1 + torch.sqrt(ratio) * in_2)
-
-
-def crossfade2D(in_1: Signal, in_2: Signal, ratio: T) -> Signal:
-    """
-    Equal power cross-fade.
-
-    TODO: Replace crossfade with this once everything is 2D
-    """
-    assert in_1.ndim == 2 and in_2.ndim == 2 and ratio.ndim == 1
-    assert torch.all(0.0 <= ratio) and torch.all(ratio <= 1.0)
-    ratio = ratio.unsqueeze(1)
-    return EQ_POW * (torch.sqrt(1 - ratio) * in_1 + torch.sqrt(ratio) * in_2)
+    return 12 * torch.log2((hz + EPS) / 440) + 69
 
 
 def linspace(start: T, stop: T, num: T, endpoint: T = False) -> T:
@@ -125,9 +127,15 @@ def linspace(start: T, stop: T, num: T, endpoint: T = False) -> T:
     return torch.linspace(start, stop, num)
 
 
-def reverse_signal(signal: T) -> T:
-    assert signal.ndim == 1
-    return torch.flip(signal, (0,))
+def midi_to_hz(midi: T) -> T:
+    """
+    Convert from midi (linear pitch) to frequency in Hz.
+    """
+    return 440.0 * (2.0 ** ((midi - 69.0) / 12.0))
+
+
+def next_power_of_two(value: int) -> int:
+    return int(math.pow(2, math.ceil(math.log2(value))))
 
 
 def normalize(signal: T) -> T:
@@ -136,25 +144,21 @@ def normalize(signal: T) -> T:
     return signal / max_
 
 
+def peak_gain_for_Q(Q: T) -> T:
+    """
+    Calculate the peak gain for a given filter quality factor.
+    """
+    # No gain added for quality factor less then 1/sqrt(2)
+    if Q <= 0.707:
+        return T(1.0)
+
+    return Q * Q / torch.pow((Q * Q - 0.25), 0.5)
+
+
+def reverse_signal(signal: T) -> T:
+    assert signal.ndim == 1
+    return torch.flip(signal, (0,))
+
+
 def sinc(x: T) -> T:
     return torch.where(x == 0, T(1.0, device=x.device), torch.sin(x) / x)
-
-
-def blackman(length: T) -> T:
-    num_samples = torch.ceil(length)
-    diff = num_samples - length
-    n = torch.arange(num_samples.detach() - (diff.detach() / 2), device=length.device)
-    cos_a = torch.cos(2 * math.pi * n / (length - 1))
-    cos_b = torch.cos(4 * math.pi * n / (length - 1))
-    window = 0.42 - 0.5 * cos_a + 0.08 * cos_b
-
-    # Linearly interpolate the ends of the window to achieve fractional length
-    window = torch.cat(
-        (
-            T([0.0 * diff + window[0] * (1.0 - diff)], device=length.device),
-            window[1:-1],
-            T([0.0 * diff + window[-1] * (1.0 - diff)], device=length.device),
-        )
-    )
-
-    return window
