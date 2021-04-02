6d3fc89de2dbefb477dff8d570898581da51a847
Jordie Shier <jordieshier@gmail.com>
2021-04-01 21:32:05 -0700

    Better globals test

--- tests/test_globals.py
+++ tests/test_globals.py
@@ -8,7 +8,11 @@ import torch.nn
 import torch.tensor as T
 
 from torchsynth.globals import SynthGlobals
-from torchsynth.default import DEFAULT_SAMPLE_RATE, DEFAULT_BUFFER_SIZE
+from torchsynth.default import (
+    DEFAULT_SAMPLE_RATE,
+    DEFAULT_BUFFER_SIZE,
+    DEFAULT_CONTROL_RATE,
+)
 
 
 def test_synth_globals():
@@ -16,14 +20,21 @@ def test_synth_globals():
     assert synthglobals.sample_rate == DEFAULT_SAMPLE_RATE
     assert synthglobals.batch_size == 64
     assert synthglobals.buffer_size == DEFAULT_BUFFER_SIZE
-    assert synthglobals.control_rate == DEFAULT_SAMPLE_RATE // 100
+    assert synthglobals.control_rate == DEFAULT_CONTROL_RATE
     assert (
         synthglobals.control_buffer_size
         == DEFAULT_BUFFER_SIZE / DEFAULT_SAMPLE_RATE * synthglobals.control_rate
     )
 
-    synthglobals = SynthGlobals(T(65), control_rate=T(1000))
+    # Test passing in specific values
+    synthglobals = SynthGlobals(
+        T(65), sample_rate=T(16000), buffer_size=T(8000), control_rate=T(1000)
+    )
     assert synthglobals.control_rate == 1000
+    assert synthglobals.sample_rate == 16000
+    assert synthglobals.buffer_size == 8000
+    assert synthglobals.control_buffer_size == 500
 
+    # Control rate must be passed in as a tensor
     with pytest.raises(AttributeError):
         SynthGlobals(T(16), control_rate=1000)
