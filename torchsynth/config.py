b97e056543ee9876d739d6f93b39269e6252b1b3
Joseph Turian <turian@gmail.com>
2021-04-09 04:42:56 +0200

    Putting stuff into config

--- torchsynth/config.py
+++ torchsynth/config.py
@@ -4,13 +4,6 @@ import torch
 import torch.tensor as tensor
 from torch import Tensor as T
 
-from torchsynth.default import (
-    DEFAULT_BUFFER_SIZE,
-    DEFAULT_CONTROL_RATE,
-    DEFAULT_SAMPLE_RATE,
-)
-
-
 class SynthConfig:
     """
     Any SynthModule and AbstractSynth might use
@@ -24,9 +17,9 @@ class SynthConfig:
     def __init__(
         self,
         batch_size: T,
-        sample_rate: Optional[T] = tensor(DEFAULT_SAMPLE_RATE),
-        buffer_size: Optional[T] = tensor(DEFAULT_BUFFER_SIZE),
-        control_rate: Optional[T] = tensor(DEFAULT_CONTROL_RATE),
+        sample_rate: Optional[T] = tensor(44100),
+        buffer_size_seconds: Optional[T] = tensor(4),
+        control_rate: Optional[T] = tensor(441),
         debug: bool = False
     ):
         """
@@ -40,11 +33,11 @@ class SynthConfig:
         """
         assert batch_size.ndim == 0
         assert sample_rate.ndim == 0
-        assert buffer_size.ndim == 0
+        assert buffer_size_seconds.ndim == 0
         assert control_rate.ndim == 0
         self.batch_size = batch_size
         self.sample_rate = sample_rate
-        self.buffer_size = buffer_size
+        self.buffer_size = int(round(buffer_size_seconds * self.sample_rate))
         self.control_rate = control_rate
 
         # Buffer size for control signals -- this is calculated to have the
