a4118086bcdbe0e2170b487094b0a2061f2985b4
Jordie Shier <jordieshier@gmail.com>
2021-04-24 17:18:01 -0700

    Parameter docstrings

--- torchsynth/module.py
+++ torchsynth/module.py
@@ -239,11 +239,6 @@ class SynthModule(nn.Module):
         """
         self.synthconfig.to(device)
         self.device = device
-        # Currently the parameters ranges themselves are not move to a different
-        # device, but each ParameterRange object is device aware if it needs to be
-        # TODO: Do ParameterRanges need to be device aware? See issue #240
-        for name, parameter in self.torchparameters.items():
-            parameter.parameter_range.to(device)
 
 
 class ControlRateModule(SynthModule):
