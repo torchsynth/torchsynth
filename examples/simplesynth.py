0e676869745f4887cc5f82674376d3d5b1fa98a1
Jordie Shier <jordieshier@gmail.com>
2021-04-26 00:57:54 -0700

    rename to simplesynth:

--- examples/simplesynth.py
+++ examples/simplesynth.py
@@ -1,6 +1,11 @@
 # ## Creating a simple synth
 #
-# In this example we'll create a new synth using torchsynth modules. Synth artchitectures can be created using a modular synthesizer paradigm by interconnecting individual modules that each have a unique function. We'll create a simple single oscillator synth with an attack-decay-sustain-release (ADSR) envelope controlling the amplitude in this example. More complicated architectures can be created using the same ideas!
+# In this example we'll create a new synth using torchsynth modules. Synth
+# artchitectures can be created using a modular synthesizer paradigm by interconnecting
+# individual modules that each have a unique function. We'll create a simple single
+# oscillator synth with an attack-decay-sustain-release (ADSR) envelope controlling
+# the amplitude in this example. More complicated architectures can be created
+# using the same ideas!
 
 # +
 from typing import Optional
@@ -23,7 +28,10 @@ from torchsynth.config import SynthConfig, BASE_REPRODUCIBLE_BATCH_SIZE
 
 # ### The SimpleSynth class
 #
-# All synths in torchsynth derive from `AbstractSynth`, which contains many helpful methods which include funcitonality for keeping track of children `SynthModule`s, creating random patches, and more. See the API documentation for `AbstractSynth` for more information.
+# All synths in torchsynth derive from `AbstractSynth`, which contains many helpful
+# methods which include funcitonality for keeping track of children `SynthModule`s,
+# creating random patches, and more. See the API documentation for `AbstractSynth` for
+# more information.
 #
 # There are two steps involved in creating a class that derives from `Abstract Synth`:
 # 1. Define the synth modules that will be used in `__init__`
@@ -91,14 +99,20 @@ class SimpleSynth(AbstractSynth):
 
 # That's out simple synth! Let's test it out now.
 #
-# If we instantiate SimpleSynth without passing in a SynthConfig object then it will create one with the default options. We don't need to render a full batch size for this example, so let's use the smallest batch size that will support reproducible output. All the parameters in a synth are randomly assigned values, with reprodcible mode on, we pass a batch_id value into our synth when calling it. The same sounds will always be returned for the same batch_id.
+# If we instantiate SimpleSynth without passing in a SynthConfig object then it will
+# create one with the default options. We don't need to render a full batch size for
+# this example, so let's use the smallest batch size that will support reproducible
+# output. All the parameters in a synth are randomly assigned values, with reprodcible
+# mode on, we pass a batch_id value into our synth when calling it. The same sounds
+# will always be returned for the same batch_id.
 
 # +
-# Create SynthConfig with smallest reproducible batch size -- reprodicible mode is on by default
+# Create SynthConfig with smallest reproducible batch size.
+# Reprodicible mode is on by default.
 synthconfig = SynthConfig(batch_size=BASE_REPRODUCIBLE_BATCH_SIZE)
 synth = SimpleSynth(synthconfig)
 
-# If you have access to a GPU
+# If you have access to a GPU.
 if torch.cuda.is_available():
     synth.to("cuda")
 # -
@@ -109,7 +123,8 @@ if torch.cuda.is_available():
 audio = synth(0)
 
 print(
-    f"Created {audio.shape[0]} synthesizer sounds that are each {audio.shape[1]} samples long"
+    f"Created {audio.shape[0]} synthesizer sounds "
+    f"that are each {audio.shape[1]} samples long"
 )
 # -
 
